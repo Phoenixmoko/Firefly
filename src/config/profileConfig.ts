6eca286ed22a93abc9caa594c7f23d4ba661d3d2
Phoenixmoko <zxc1558695@gmail.com>
2026-05-27 23:01:59 +0800

    这是我的博客

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -9,10 +9,10 @@ export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/avatar.avif",
 
 	// 名字
-	name: "Firefly",
+	name: "故乡之星",
 
 	// 个人签名
-	bio: "Hello, I'm Firefly.",
+	bio: "而今迈步从头越",
 
 	// 链接配置
 	// 已经预装的图标集：fa7-brands，fa7-regular，fa7-solid，material-symbols，simple-icons
